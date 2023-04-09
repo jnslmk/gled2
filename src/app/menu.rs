2666a541bbd9ad736e3904ae806ac0b2e6c24f35
René Rössler <rene@freshx.de>
2023-04-09 04:40:39 +0200

    keyboard shortcuts

--- src/app/menu.rs
+++ src/app/menu.rs
@@ -2,7 +2,10 @@ use crate::artnet_sender;
 
 use super::{svg::Svg, App};
 use eframe::Frame;
-use egui::{Button, Color32, Context, ImageButton, RichText, Ui, Vec2};
+use egui::{
+    text::LayoutJob, Button, Color32, Context, ImageButton, Key, Modifiers, RichText, Stroke,
+    TextFormat, Ui, Vec2,
+};
 use log::{error, info};
 
 impl App {
@@ -23,43 +26,86 @@ impl App {
                 self.about_window_open = true;
             };
 
+            let mut create_new_project = ctx.input_mut(|i| i.consume_key(Modifiers::CTRL, Key::N));
+            let mut open_project = ctx.input_mut(|i| i.consume_key(Modifiers::CTRL, Key::O));
+            let mut save_project = ctx.input_mut(|i| i.consume_key(Modifiers::CTRL, Key::S));
+            let mut save_project_as =
+                ctx.input_mut(|i| i.consume_key(Modifiers::CTRL.plus(Modifiers::SHIFT), Key::S));
+            let mut open_svg_file =
+                ctx.input_mut(|i| i.consume_key(Modifiers::CTRL.plus(Modifiers::SHIFT), Key::O));
+
             ui.menu_button("File", |ui| {
-                if ui.button("Create new project").clicked() {
-                    todo!()
+                if ui
+                    .add(Button::new("Create new project").shortcut_text("Ctrl + N"))
+                    .clicked()
+                {
+                    create_new_project = true;
                 }
-                if ui.button("Open project").clicked() {
-                    todo!()
+
+                if ui
+                    .add(Button::new("Open project").shortcut_text("Ctrl + O"))
+                    .clicked()
+                {
+                    open_project = true;
                 }
-                if ui.button("Save project").clicked() {
-                    todo!()
+
+                if ui
+                    .add(Button::new("Save project").shortcut_text("Ctrl + S"))
+                    .clicked()
+                {
+                    save_project = true;
                 }
-                if ui.button("Save project as..").clicked() {
-                    todo!()
+
+                if ui
+                    .add(Button::new("Save project as..").shortcut_text("Ctrl + Shift + S"))
+                    .clicked()
+                {
+                    save_project_as = true;
                 }
-                if ui.button("Open SVG file").clicked() {
-                    if let Some(path) = rfd::FileDialog::new()
-                        .set_title("Open SVG file")
-                        .add_filter("svg", &["svg"])
-                        .pick_file()
-                    {
-                        self.svg = match Svg::load(
-                            frame
-                                .wgpu_render_state()
-                                .expect("Could not find wgpu render state"),
-                            &path,
-                        ) {
-                            Ok(svg) => {
-                                info!("Loaded svg file \"{}\"", path.display());
-                                Some(svg)
-                            }
-                            Err(err) => {
-                                error!("Could not load svg file \"{}\": {err:?}", path.display());
-                                None
-                            }
-                        };
-                    }
+
+                if ui
+                    .add(Button::new("Open SVG file").shortcut_text("Ctrl + Shift + O"))
+                    .clicked()
+                {
+                    open_svg_file = true;
                 }
             });
+
+            if create_new_project {
+                todo!();
+            }
+            if open_project {
+                todo!();
+            }
+            if save_project {
+                todo!();
+            }
+            if save_project_as {
+                todo!();
+            }
+            if open_svg_file {
+                if let Some(path) = rfd::FileDialog::new()
+                    .set_title("Open SVG file")
+                    .add_filter("svg", &["svg"])
+                    .pick_file()
+                {
+                    self.svg = match Svg::load(
+                        frame
+                            .wgpu_render_state()
+                            .expect("Could not find wgpu render state"),
+                        &path,
+                    ) {
+                        Ok(svg) => {
+                            info!("Loaded svg file \"{}\"", path.display());
+                            Some(svg)
+                        }
+                        Err(err) => {
+                            error!("Could not load svg file \"{}\": {err:?}", path.display());
+                            None
+                        }
+                    };
+                }
+            }
 
             ui.menu_button("Config", |ui| {
                 ui.label(RichText::new("frame rate limiter").text_style(egui::TextStyle::Heading));
@@ -85,16 +131,26 @@ impl App {
             ui.add(egui::Slider::new(&mut self.timing.beats_per_minute, 1.0..=240.0).text("bpm"));
 
             ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                let mut blackout = Button::new("Blackout");
+                let underlined = TextFormat {
+                    underline: Stroke::new(1.0, Color32::GRAY),
+                    ..Default::default()
+                };
+                let mut blackout_text = LayoutJob::default();
+                blackout_text.append("B", 0.0, underlined);
+                blackout_text.append("lackout", 0.0, TextFormat::default());
+                let mut blackout = Button::new(blackout_text);
                 if self.blackout {
                     blackout = blackout.fill(Color32::DARK_RED);
                 }
-                if ui.add_sized(menu_button_size, blackout).clicked() {
+                if ui.add_sized(menu_button_size, blackout).clicked()
+                    || !ctx.wants_keyboard_input()
+                        && ctx.input_mut(|i| i.consume_key(Modifiers::NONE, Key::B))
+                {
                     self.blackout = !self.blackout;
                 }
 
-                self.timing.freeze_button(ui, menu_button_size);
-                self.timing.beat_button(ui, menu_button_size);
+                self.timing.freeze_button(ctx, ui, menu_button_size);
+                self.timing.beat_button(ctx, ui, menu_button_size);
                 ui.separator();
             });
         });
--- src/app/timing.rs
+++ src/app/timing.rs
@@ -1,4 +1,4 @@
-use egui::{Button, Color32, Ui, Vec2};
+use egui::{text::LayoutJob, Button, Color32, Key, Modifiers, Stroke, TextFormat, Ui, Vec2};
 use floating_duration::TimeAsFloat;
 use log::debug;
 use std::time::{Duration, Instant};
@@ -69,7 +69,7 @@ impl Timing {
         }
     }
 
-    pub fn beat_button(&mut self, ui: &mut Ui, menu_button_size: Vec2) {
+    pub fn beat_button(&mut self, ctx: &egui::Context, ui: &mut Ui, menu_button_size: Vec2) {
         let beat_progression = self.beat_progression();
         let mut alpha = None;
         if beat_progression < 0.10 {
@@ -79,11 +79,25 @@ impl Timing {
         } else if beat_progression > 0.9 {
             alpha = Some((beat_progression - 0.9) * 200.0);
         }
-        let mut tap = Button::new("TAP");
+
+        let underlined = TextFormat {
+            underline: Stroke::new(1.0, Color32::GRAY),
+            ..Default::default()
+        };
+        let mut tap_text = LayoutJob::default();
+        tap_text.append("T", 0.0, underlined);
+        tap_text.append("ap", 0.0, TextFormat::default());
+        let mut tap = Button::new(tap_text);
         if let Some(alpha) = alpha {
             tap = tap.fill(Color32::from_white_alpha(alpha as u8));
         }
-        if ui.add_sized(menu_button_size, tap).clicked() {
+        if ui.add_sized(menu_button_size, tap).clicked()
+            || !ctx.wants_keyboard_input()
+                && ctx.input_mut(|i| {
+                    i.consume_key(Modifiers::NONE, Key::Space)
+                        || i.consume_key(Modifiers::NONE, Key::T)
+                })
+        {
             let now = Instant::now();
             self.beats_per_minute =
                 60000.0 / now.duration_since(self.tap).as_fractional_millis() as f32;
@@ -91,12 +105,23 @@ impl Timing {
         }
     }
 
-    pub fn freeze_button(&mut self, ui: &mut Ui, menu_button_size: Vec2) {
-        let mut freeze = Button::new("Freeze");
+    pub fn freeze_button(&mut self, ctx: &egui::Context, ui: &mut Ui, menu_button_size: Vec2) {
+        let underlined = TextFormat {
+            underline: Stroke::new(1.0, Color32::GRAY),
+            ..Default::default()
+        };
+        let mut freeze_text = LayoutJob::default();
+        freeze_text.append("F", 0.0, underlined);
+        freeze_text.append("reeze", 0.0, TextFormat::default());
+
+        let mut freeze = Button::new(freeze_text);
         if self.freeze {
             freeze = freeze.fill(Color32::DARK_RED);
         }
-        if ui.add_sized(menu_button_size, freeze).clicked() {
+        if ui.add_sized(menu_button_size, freeze).clicked()
+            || !ctx.wants_keyboard_input()
+                && ctx.input_mut(|i| i.consume_key(Modifiers::NONE, Key::F))
+        {
             self.freeze = !self.freeze;
         }
     }
