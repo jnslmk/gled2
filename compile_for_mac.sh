efd966bf07c9c1b4e69c942017a513780d2976e1
René Rössler <rene@freshx.de>
2023-04-11 13:13:24 +0200

    add compile script for mac

---
+++ compile_for_mac.sh
@@ -0,0 +1,1 @@
+cargo build --release --target x86_64-apple-darwin && cargo build --release --target=aarch64-apple-darwin && lipo -create gled target/aarch64-apple-darwin/release/gled target/x86_64-apple-darwin/release/gled
