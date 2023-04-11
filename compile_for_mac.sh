e59a987ea3b894518a805009d90f8018fd8d4913
René Rössler <rene@freshx.de>
2023-04-11 13:26:42 +0200

    mac app folder

--- compile_for_mac.sh
+++ compile_for_mac.sh
@@ -1,1 +1,3 @@
-cargo build --release --target x86_64-apple-darwin && cargo build --release --target=aarch64-apple-darwin && lipo -create gled target/aarch64-apple-darwin/release/gled target/x86_64-apple-darwin/release/gled
+cargo build --release --target x86_64-apple-darwin
+cargo build --release --target=aarch64-apple-darwin
+lipo -create Gled.app/Contens/MacOS/gled target/aarch64-apple-darwin/release/gled target/x86_64-apple-darwin/release/gled
