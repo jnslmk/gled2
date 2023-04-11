3324b96d0af58d6f89d656c49282481bed5f58a4
René Rössler <rene@freshx.de>
2023-04-11 13:31:33 +0200

    create dmg

--- compile_for_mac.sh
+++ compile_for_mac.sh
@@ -1,3 +1,18 @@
 cargo build --release --target x86_64-apple-darwin
 cargo build --release --target=aarch64-apple-darwin
 lipo -create Gled.app/Contens/MacOS/gled target/aarch64-apple-darwin/release/gled target/x86_64-apple-darwin/release/gled
+rm -rf /tmp/gled
+mkdir /tmp/gled
+cp -r Gled.app /tmp/gled/
+brew install create-dmg
+create-dmg \
+    --volname "Gled" \
+    --volicon "Gled.app/Contents/Resources/icon.icns" \
+    --window-pos 200 120 \
+    --window-size 800 400 \
+    --icon-size 100 \
+    --icon "Gled.app" 200 190 \
+    --hide-extension "Gled.app" \
+    --app-drop-link 600 185 \
+    "Gled.dmg" \
+    "/tmp/gled/"
