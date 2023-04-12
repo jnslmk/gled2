8211f7474f307b67b5d1d89f8d19234ef41dc8af
René Rössler <rene@freshx.de>
2023-04-13 00:55:08 +0200

    resizeable preview area

--- src/app/preview.rs
+++ src/app/preview.rs
@@ -1,6 +1,6 @@
 use super::App;
 use crate::app::preview_positions;
-use egui::{Align, Color32, Context, Image, Layout, RichText, Slider, Vec2};
+use egui::{Align, Color32, Context, Image, Layout, RichText, Vec2};
 
 const BORDER: f32 = 0.03;
 
@@ -14,70 +14,55 @@ impl App {
 
             egui::TopBottomPanel::top("preview")
                 .default_height(300.0)
-                .exact_height(if self.show_preview {
-                    self.preview_size
-                } else {
-                    10.0
-                })
-                .resizable(false)
+                .min_height(50.0)
+                .resizable(true)
                 .show(ctx, |ui| {
                     ui.horizontal(|ui| {
-                        ui.checkbox(&mut self.show_preview, RichText::new("Preview").heading());
+                        ui.label(RichText::new("Preview").heading());
                         ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
                             ui.checkbox(&mut self.show_preview_svg, RichText::new("SVG").heading());
-                            if self.show_preview {
-                                ui.add(
-                                    Slider::new(&mut self.preview_size, 100.0..=512.0)
-                                        .show_value(false)
-                                        .text(RichText::new("Size").heading()),
-                                );
-                            }
                         });
                     });
 
-                    if self.show_preview {
-                        ui.horizontal(|ui| {
-                            ui.set_min_height(self.preview_size);
+                    crate::get_pipeline!(pipeline);
+                    let size = if uv.max.x > uv.max.y {
+                        Vec2::new(
+                            ui.available_width()
+                                .min(ui.available_height() * uv.max.x / uv.max.y),
+                            ui.available_height()
+                                .min(ui.available_width() * uv.max.y / uv.max.x),
+                        )
+                    } else {
+                        Vec2::new(
+                            ui.available_width()
+                                .min(ui.available_height() * uv.max.y / uv.max.x),
+                            ui.available_height()
+                                .min(ui.available_width() * uv.max.x / uv.max.y),
+                        )
+                    };
 
-                            crate::get_pipeline!(pipeline);
-
-                            let size = if uv.max.x > uv.max.y {
-                                Vec2::new(
-                                    ui.available_height() * uv.max.x / uv.max.y,
-                                    ui.available_height(),
-                                )
-                            } else {
-                                Vec2::new(
-                                    ui.available_height() * uv.max.y / uv.max.x,
-                                    ui.available_height(),
-                                )
-                            };
-
-                            let res =
-                                self.svg
-                                    .as_ref()
-                                    .filter(|_| self.show_preview_svg)
-                                    .map(|svg| {
-                                        ui.add(
-                                            Image::new(svg.image().texture_id(ctx), size)
-                                                .uv(uv)
-                                                .bg_fill(Color32::BLACK),
-                                        )
-                                    });
-                            let mut preview =
-                                Image::new(pipeline.preview_texture_id(), size).uv(uv);
-                            if !self.show_preview_svg {
-                                preview = preview.bg_fill(Color32::BLACK);
-                            }
-                            match res {
-                                Some(res) => {
-                                    ui.put(res.rect, preview);
-                                }
-                                None => {
-                                    ui.add(preview);
-                                }
-                            }
+                    let res = self
+                        .svg
+                        .as_ref()
+                        .filter(|_| self.show_preview_svg)
+                        .map(|svg| {
+                            ui.add(
+                                Image::new(svg.image().texture_id(ctx), size)
+                                    .uv(uv)
+                                    .bg_fill(Color32::BLACK),
+                            )
                         });
+                    let mut preview = Image::new(pipeline.preview_texture_id(), size).uv(uv);
+                    if !self.show_preview_svg {
+                        preview = preview.bg_fill(Color32::BLACK);
+                    }
+                    match res {
+                        Some(res) => {
+                            ui.put(res.rect, preview);
+                        }
+                        None => {
+                            ui.add(preview);
+                        }
                     }
                 });
         }
