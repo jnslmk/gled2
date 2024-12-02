dd998207ae0d52e21fcceb9b350ccb8c7ff6fb40
René Rössler <rene@freshx.de>
2024-12-02 23:58:39 +0100

    refine release building

--- bundle.sh
+++ bundle.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+rm -rf bin
+mkdir -p bin
+
 cargo install cargo-edit --locked
 cargo install cargo-packager --locked
 
@@ -12,16 +15,16 @@ if [[ $(uname) == "Linux" ]]; then
 
     # RPM
     cargo build --release
-    cargo generate-rpm -o gled-${CI_COMMIT_TAG_VERSION}.x86_64.rpm
+    strip target/release/gled
+    cargo generate-rpm -o bin/gled-${CI_COMMIT_TAG_VERSION}.x86_64.rpm
 
     # Windows exe
     cargo-xwin build --target x86_64-pc-windows-msvc --release
-    cargo packager --target x86_64-pc-windows-msvc --release -o . -f nsis
-    mv gled_${CI_COMMIT_TAG_VERSION}_x64-setup.exe gled-${CI_COMMIT_TAG_VERSION}-setup.exe
+    cargo packager --target x86_64-pc-windows-msvc --release -o bin -f nsis
 fi
 
 # Macos dmg
 if [[ $(uname) == "Darwin" ]]; then
     cargo build --release
-    cargo packager --release -o . -f dmg
+    cargo packager --release -o bin -f dmg
 fi
