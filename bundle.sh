e88b6e82e6ae78b43b85cb1c8bd925af857169c7
René Rössler <rene@freshx.de>
2024-12-03 00:32:57 +0100

    fix and cleanup bundle script

--- bundle.sh
+++ bundle.sh
@@ -2,30 +2,16 @@
 
 set -e
 
-echo "test" >gled-${CI_COMMIT_TAG}.x86_64.rpm
-echo "test" >gled_${CI_COMMIT_TAG}_x64-setup.exe
-exit 0
+cargo install cargo-edit cargo-packager cargo-generate-rpm cargo-xwin --locked
 
-cargo install cargo-edit --locked
-cargo install cargo-packager --locked
+echo "Setting version to ${CI_COMMIT_TAG}"
+cargo set-version ${CI_COMMIT_TAG}
 
-if [[ $(uname) == "Linux" ]]; then
-    cargo install cargo-generate-rpm --locked
-    cargo install cargo-xwin --locked
-    cargo set-version ${CI_COMMIT_TAG_VERSION}
+# RPM
+cargo build --release
+strip target/release/gled
+cargo generate-rpm -o gled-${CI_COMMIT_TAG}.x86_64.rpm
 
-    # RPM
-    cargo build --release
-    strip target/release/gled
-    cargo generate-rpm -o gled-${CI_COMMIT_TAG_VERSION}.x86_64.rpm
-
-    # Windows exe
-    cargo-xwin build --target x86_64-pc-windows-msvc --release
-    cargo packager --target x86_64-pc-windows-msvc --release -o . -f nsis
-fi
-
-# Macos dmg
-if [[ $(uname) == "Darwin" ]]; then
-    cargo build --release
-    cargo packager --release -o . -f dmg
-fi
+# Windows exe
+cargo-xwin build --target x86_64-pc-windows-msvc --release
+cargo packager --target x86_64-pc-windows-msvc --release -o . -f nsis
