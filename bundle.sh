c3f56f7a22d432e184689bf72d52f20e8092c582
René Rössler <rene@freshx.de>
2024-12-03 00:56:39 +0100

    build with release

--- bundle.sh
+++ bundle.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-
-set -e
-
-# todo: remove
-cargo build
-exit 1
+set -xe
 
 cargo install cargo-edit cargo-packager cargo-generate-rpm cargo-xwin --locked
 
 echo "Setting version to ${CI_COMMIT_TAG}"
 cargo set-version ${CI_COMMIT_TAG}
 
-# RPM
+echo "Building Linux rpm"
 cargo build --release
 strip target/release/gled
 cargo generate-rpm -o gled-${CI_COMMIT_TAG}.x86_64.rpm
 
-# Windows exe
+echo "Building Windows exe"
 cargo-xwin build --target x86_64-pc-windows-msvc --release
 cargo packager --target x86_64-pc-windows-msvc --release -o . -f nsis
