46fe7b56e6c8015140f9be93dba2008b2cecbacf
René Rössler <rene@freshx.de>
2024-12-03 11:08:12 +0100

    add code for darwin

--- bundle.sh
+++ bundle.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 set -xe
 
-cargo install cargo-edit cargo-packager cargo-generate-rpm cargo-xwin --locked
+cargo install cargo-edit cargo-packager --locked
 
 echo "Setting version to ${CI_COMMIT_TAG}"
 cargo set-version ${CI_COMMIT_TAG}
 
-echo "Building Linux rpm"
-cargo build --release
-strip target/release/gled
-cargo generate-rpm -o gled-${CI_COMMIT_TAG}.x86_64.rpm
+if [[ $(uname) == "Linux" ]]; then
+    cargo install cargo-generate-rpm cargo-xwin --locked
 
-echo "Building Windows exe"
-cargo-xwin build --target x86_64-pc-windows-msvc --release
-cargo packager --target x86_64-pc-windows-msvc --release -o . -f nsis
+    echo "Building Linux rpm"
+    cargo build --release
+    strip target/release/gled
+    cargo generate-rpm -o gled-${CI_COMMIT_TAG}.x86_64.rpm
+
+    echo "Building Windows exe"
+    cargo-xwin build --target x86_64-pc-windows-msvc --release
+    cargo packager --target x86_64-pc-windows-msvc --release -o . -f nsis
+fi
+
+if [[ $(uname) == "Darwin" ]]; then
+    echo "Building Darwin dmg"
+    cargo build --target aarch64-apple-darwin --release
+    strip target/aarch64-apple-darwin/release/gled
+    cargo packager --target aarch64-apple-darwin --release -o . -f dmg
+fi
