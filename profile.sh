f24013459fc2b568bd94c4df4551c056a5a453f8
René Rössler <rene@freshx.de>
2024-12-22 11:50:07 +0100

    add profiling profile and profile script

---
+++ profile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "We need to enable system perf events, so please enter your user password:"
+echo '1' | sudo tee /proc/sys/kernel/perf_event_paranoid
+
+echo "Installing samply"
+cargo install --locked samply
+
+echo "Building gled with profiling profile"
+cargo build --profile profiling
+
+echo "Running gled with samply"
+samply record ./target/profiling/gled
