b268a4ffd5629e2eace8fa09f4f92e0b3f4b0204
René Rössler <rene@freshx.de>
2024-12-24 10:49:40 +0100

    follow redirect

--- .cd/create_book.sh
+++ .cd/create_book.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-curl "https://objects.githubusercontent.com/github-production-release-asset-2e65be/38655056/fa65099c-dba9-4076-8de0-19ccbdbe843f?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20241224%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241224T090808Z&X-Amz-Expires=300&X-Amz-Signature=363ec9224a3daa745026afa5b57060f0a8269271b25fadef0a96cbab6dfc5cf3&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3Dmdbook-v0.4.43-x86_64-unknown-linux-gnu.tar.gz&response-content-type=application%2Foctet-stream" -o mdbook.tar.gz
+curl -L "https://github.com/rust-lang/mdBook/releases/download/v0.4.43/mdbook-v0.4.43-x86_64-unknown-linux-gnu.tar.gz" -o mdbook.tar.gz
 tar xvfz mdbook.tar.gz
 ./mdbook build ./book -d ../public
