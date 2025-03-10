8690c8f4dfbf877626393b0f35295dbc058b333c
René Rössler <rene@freshx.de>
2025-03-10 11:19:33 +0100

    add release_patch script

---
+++ release_patch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+cargo install cargo-edit --locked
+VERSION=$(cargo set-version --bump patch 2>&1 | awk {'print $6'})
+
+git commit Cargo.toml Cargo.lock -m "bump version"
+git push
+
+git tag $VERSION
+git push --tag
